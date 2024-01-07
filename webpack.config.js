00b61bb936b84f57c6167369ef991366db6a1c2a
3LOK <yoavamit@yahoo.com>
2024-01-07 21:58:37 +0200

    Webpacking before releasing

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+const libraryName= pkg.name;
+module.exports = {
+    entry: path.join(__dirname, "./src/index.js"),
+    mode: 'production',
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'index.js',
+        library: libraryName,
+        libraryTarget: 'umd',
+        publicPath: '/dist/',
+        umdNamedDefine: true
+    },
+    module: {
+        rules : [
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: true,
+                    },
+                },
+            ],
+        },
+        {
+            test: /\.(js|jsx)$/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            include: path.resolve(__dirname, "src"),
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.(eot|ttf|woff|woff2)$/,
+            use: ["file-loader"],
+        },
+        {
+            test: /\.(pdf|doc|zip)$/,
+            use: ["file-loader"],
+        }]
+    },
+    resolve: { 
+        alias: { 
+            'react': path.resolve(__dirname, './node_modules/react') ,
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+            'assets': path.resolve(__dirname, 'assets')
+        } 
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        }
+    }
+};
